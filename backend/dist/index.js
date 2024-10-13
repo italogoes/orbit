1f05de510ae7229f08ebb861abce9b672d2cbd30
Italo Goes <italo.goes@geosiga.com.br>
2024-10-12 21:35:00 -0300

    server config

--- backend/dist/index.js
+++ backend/dist/index.js
@@ -1,3 +1,14 @@
 "use strict";
-const nome = "italo";
-console.log(nome);
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const port = process.env.PORT || 5000;
+app.get('/', (req, res) => {
+    res.send("Ligado");
+});
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,2 +1,16 @@
-const nome: string = "italo";
-console.log(nome);
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+import router from "./routes/Routes";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.use(express.json());
+
+app.use('/', router)
+
+app.listen(port, ()=> {
+    console.log(`Server is running on http://localhost:${port}`);
+})
